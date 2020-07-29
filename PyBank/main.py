c5d3b4477d7f1cce0cfd8a1b2ed0a512cd24f9ec
sjplaza <sjplaza79@gmail.com>
2020-07-28 22:02:12 -0500

    Adding homework

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,39 +1,65 @@
 import os
 import csv
-total_months = 0
-net_total = 0
 
 budget_csv = os.path.join('Resources', 'budget_data.csv')
 
+total_months = 0
+net_total = 0
 
-def acctg_data(budget_data):
-    date = str(budget_data[0])
-    total = int(budget_data[1])
+profit_loss = []
+average_change = []
+date = []
 
-
-with open(budget_csv) as csvfile:
+with open(budget_csv, 'r') as csvfile:
     budget_csv = csv.reader(csvfile, delimiter=',')
-    csv_header = next(budget_csv)
-    print(f'CSV Header: {csv_header}')
+    header = next(budget_csv)
+    file_to_output = "Analysis/budget_analysis.txt"
 
-# The total number of months included in the dataset
     for row in budget_csv:
+
+        # The total number of months included in the dataset
         total_months = total_months + 1
 
 # The net total amount of "Profit/Losses" over the entire period
-    for col in budget_csv:
-        net_total = sum(budget_data[1])
+        net_total = net_total + int(row[1])
+        profit_loss.append(row[1])
+        date.append(row[0])
 
 # The average of the changes in "Profit/Losses" over the entire period
+    for i in range(len(profit_loss) - 1):
+        avg_period_change = int(profit_loss[i + 1]) - int(profit_loss[i])
+        average_change.append(avg_period_change)
 
+    change = abs(sum(average_change)/len(average_change))
 
-# The greatest increase in profits(date and amount) over the entire period
+# The greatest increase in profits (date and amount) over the entire period
+    max_change = max(average_change)
+    max_change_date = str(date[average_change.index(max(average_change))])
 
+# The greatest decrease in losses (date and amount) over the entire period
+    min_change = min(average_change)
+    min_change_date = str(date[average_change.index(min(average_change))])
 
-# The greatest decrease in losses(date and amount) over the entire period
+    print(f'')
+    print(f'Financial Analysis')
+    print(f'-----------------------')
+    print(f'Total Months: ' + str(total_months))
+    print(f'Total: ' + '$' + str(net_total))
+    print(f'Average Change: ' + '$' + str(change))
+    print(f'Greatest Increase in Profits: ' +
+          str(max_change_date) + ' ($' + str(max_change) + ')')
+    print(f'Greatest Decrease in Profits: ' +
+          str(min_change_date) + ' ($' + str(min_change) + ')')
 
-print(f'')
-print(f'Financial Analysis')
-print(f'------------------------')
-print(f'Total months: {total_months}')
-print(f'Net total: {net_total}')
+with open(file_to_output, 'w') as txt_file:
+    txt_file.write('Total Months: ' + str(total_months))
+    txt_file.write('\n')
+    txt_file.write('Total: ' + '$' + str(net_total))
+    txt_file.write('\n')
+    txt_file.write('Average Change: ' + '$' + str(change))
+    txt_file.write('\n')
+    txt_file.write('Greatest Increase in Profits: ' +
+                   str(max_change_date) + ' ($' + str(max_change) + ')')
+    txt_file.write('\n')
+    txt_file.write('Greatest Decrease in Profits: ' +
+                   str(min_change_date) + ' ($' + str(min_change) + ')')
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,33 +1,68 @@
 import os
 import csv
 
+election_csv = os.path.join('Resources', 'election_data.csv')
+
 total_votes = 0
 
-csvpath = os.path.join('Resources', 'election_data.csv')
-
-with open(csvpath) as csvfile:
-    poll_data = csv.reader(csvfile, delimiter=',')
-    csv_header = next(poll_data)
-    print(f"CSV Header: {csv_header}")
-
-# The total number of votes cast
-    for row in poll_data:
-        total_votes = total_votes + 1
-
 # A complete list of candidates who received votes
-
-
-# The percentage of votes each candidate won
-
+candidates = []
 
 # The total number of votes each candidate won
+num_votes = []
 
+# The percentage of votes each candidate won
+percent_votes = []
+
+with open(election_csv, 'r') as csvfile:
+    election_csv = csv.reader(csvfile, delimiter=',')
+    header = next(election_csv)
+    file_to_output = "Analysis/election_analysis.txt"
+
+    for row in election_csv:
+
+        # The total number of votes cast
+        total_votes = total_votes + 1
+
+# Find the candidate and count their vote
+        if row[2] not in candidates:
+            candidates.append(row[2])
+            index = candidates.index(row[2])
+            num_votes.append(1)
+        else:
+            index = candidates.index(row[2])
+            num_votes[index] += 1
+
+# Add the percentage of votes for each candidate
+    for votes in num_votes:
+        percentage = (votes / total_votes) * 100
+        percentage = round(percentage)
+        percentage = "%.3f%%" % percentage
+        percent_votes.append(percentage)
 
 # The winner of the election based on popular vote.
+    winner = max(num_votes)
+    index = num_votes.index(winner)
+    winning_cand = candidates[index]
 
+    print(f'')
+    print(f'Election Results')
+    print(f'-----------------------')
+    print(f'Total Votes: {str(total_votes)}')
+    print(f'-----------------------')
+    for i in range(len(candidates)):
+        print(f'{candidates[i]}: {str(percent_votes[i])}  {str(num_votes[i])}')
+    print(f'-----------------------')
+    print(f'Winner: {winning_cand}')
+    print(f'-----------------------')
 
-print(f'')
-print(f'Election Results')
-print(f'---------------------')
-print(f'Total Votes: {total_votes}')
-print(f'---------------------')
+with open(file_to_output, 'w') as txt_file:
+    txt_file.write('Election Results')
+    txt_file.write('\n')
+    txt_file.write(str(f'Total Votes: {str(total_votes)}'))
+    txt_file.write('\n')
+    for i in range(len(candidates)):
+        txt_file.write(
+            str(f'{candidates[i]}: {str(percent_votes[i])}  {str(num_votes[i])}'))
+        txt_file.write('\n')
+    txt_file.write(str(f'Winner: {winning_cand}'))
